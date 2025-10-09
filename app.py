2cd9e81f802d5e6f001fab03419fd4ad15e230ef
Joaquin VincentDaGrace <joaquin.vincent-dagrace@hgs.hiddengeniusproject.org>
2025-10-08 18:10:19 -0700

    index.html

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
+@app.route("/greet/<name>")
+def GreetPage(name):
+    return render_template("index.html", name=name)
+
 if __name__ == '__main__':
     app.run(debug = True)
