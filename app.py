6dbb61ef6129b75cd8a2e506040767a8de000ae2
Joaquin VincentDaGrace <joaquin.vincent-dagrace@hgs.hiddengeniusproject.org>
2025-12-17 18:23:17 -0800

    full done

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home page route
 @app.route('/')
 def index():
     return render_template("index.html")
 
+# About Me page route
+@app.route('/about-me')
+def about_me():
+    return render_template("about_me.html")
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
